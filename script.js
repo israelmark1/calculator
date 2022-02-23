7a3a5f49eebb22b7b29be4e225e199dc96eb9b6b
israelmark1 <israelmark98@gmail.com>
2022-02-23 18:24:14 +0200

    update

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 class Calculator{
-    constructor(previousTextElement,currentTextElement){
+    constructor(previousTextElement,currentTextElement,resultTextElement){
+
         this.previousOperandTextElement = previousTextElement
         this.currentOperandTextElement = currentTextElement
-        this.clear()
+        this.resultTextElement = resultTextElement
+        this.lastNumber
+        this.allClear()
     
     }
 
-clear(){
+allClear(){
     this.currentOperand = '0'
     this.previuosOperand = ''
     this.operation = undefined
     this.previousOperandTextElement.innerText = ''
-   
+    this.resultTextElement.innerText = ''
+    this.lastNumber = undefined
+}
+
+clear(){
+    this.currentOperand = '0'
 }
 
 delete(){
@@ -25,20 +33,29 @@ appendNumber(number){
 }
 
 chooseOperation(operator){
-if(this.currentOperand === "0")return
+
+    this.resultTextElement.innerText += this.getDisplayNumber(this.currentOperand) + operator
 if(this.previuosOperand !== ""){
     this.compute()
 }
 this.operation = operator
 this.previuosOperand = this.currentOperand
-this.currentOperand = ""
+this.currentOperand = 0
+
 }
 
+
+
 compute(){
+
 let computation 
 const prev = parseFloat(this.previuosOperand)
 const cur = parseFloat(this.currentOperand)
+
+
 if(isNaN(prev) ||isNaN(cur))return
+
+this.lastNumber = cur
 switch(this.operation){
     case '+':
     computation = prev + cur
@@ -55,15 +72,19 @@ switch(this.operation){
     default:
         return
 }
+
+
 this.currentOperand = computation
 this.operation = undefined
 this.previuosOperand = ""
+
 }
 updateDisplay(){
     this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
     if(this.operation != null){
-        this.previousOperandTextElement.innerText =  this.getDisplayNumber(this.previuosOperand) + " " + this.operation
+        this.previousOperandTextElement.innerText =  this.getDisplayNumber(this.previuosOperand) 
     }
+   
 }
 
 
@@ -71,12 +92,15 @@ getDisplayNumber(number){
     const stringNumber = number.toString()
     const integerDigits = parseFloat(stringNumber.split('.')[0])
     const decimalDigits = stringNumber.split(".")[1]
+
     let integerDisplay
+
     if(isNaN(integerDigits)) integerDisplay = ""
     else integerDisplay = integerDigits.toLocaleString("en", {maximumFractionDigits: 0})
     if(decimalDigits == null) return integerDisplay
     return `${integerDisplay}.${decimalDigits}`
 }
+
 }
 
 const numberButtons = document.querySelectorAll("#number")
@@ -84,11 +108,14 @@ const operationButtons = document.querySelectorAll('#operatoin')
 const equalsButton = document.querySelector('#equals')
 const deleteButton = document.querySelector('#delete')
 const allClearButton = document.querySelector('#all-clear')
+const clearButton = document.querySelector('#clear')
 const previousOperandTextElement = document.querySelector('#previous-operand')
 const currentOperandTextElement = document.querySelector('#current-operand')
+const result = document.querySelector('#result')
+
 
 const calculator = new Calculator(previousOperandTextElement,
-    currentOperandTextElement)
+    currentOperandTextElement,result)
 
     numberButtons.forEach(button => {
         button.addEventListener('click',() => {
@@ -107,14 +134,23 @@ const calculator = new Calculator(previousOperandTextElement,
  equalsButton.addEventListener("click", () => {
     calculator.compute()
     calculator.updateDisplay()
+    if(calculator.lastNumber != undefined){
+    calculator.resultTextElement.innerText += calculator.lastNumber + " ="
+    calculator.previousOperandTextElement.innerText = ""
+    calculator.lastNumber = undefined
+    }
 })
 
  
     allClearButton.addEventListener("click", () => {
-        calculator.clear()
+        calculator.allClear()
         calculator.updateDisplay()
     })
 
+    clearButton.addEventListener("click", () => {
+        calculator.clear()
+        calculator.updateDisplay()
+    })
 
 
     deleteButton.addEventListener("click", () => {
